3c5ede76ddbf6bef0f3ea63d6a94bcb656aeb665
Yohaan Chokhany <yohaanchokhany@gmail.com>
2024-11-17 23:37:52 -0500

    MLP final update

--- jane-street-competition/utils.py
+++ jane-street-competition/utils.py
@@ -152,3 +152,66 @@ def r2_loss(gt: pd.DataFrame, predictions: pd.DataFrame) -> int:
     denominator = torch.sum(weights * y_true**2)
     r2_loss = 1 - numerator / denominator
     return r2_loss.item()
+
+
+class Linear:
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out)) / fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+
+    def parameters(self):
+        return [self.weight] + ([self.bias] if self.bias is not None else [])
+
+
+class BatchNorm1d:
+
+    def __init__(self, dim, eps=1e-8, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+
+    def __call__(self, x):
+        if self.training:
+            xmean = x.mean(0, keepdim=True)
+            xvar = x.var(0, keepdim=True)
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+        self.out = self.gamma * xhat + self.beta
+
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (
+                    1 - self.momentum
+                ) * self.running_mean + self.momentum * xmean
+                self.running_var = (
+                    1 - self.momentum
+                ) * self.running_var + self.momentum * xvar
+
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+
+class Tanh:
+    def __call__(self, x):
+        self.out = x.tanh()
+        return self.out
+
+    def parameters(self):
+        return []
